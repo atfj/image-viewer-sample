fcb470964a236b3aacda563405a685ab357544c8
atfj <a9fujita@gmail.com>
2025-07-13 15:21:19 +0900

    Add searchbar

--- ImageViewerSample/ImageSearchView.swift
+++ ImageViewerSample/ImageSearchView.swift
@@ -14,6 +14,8 @@ struct Photo: Identifiable {
 }
 
 struct ImageSearchView: View {
+    @State private var searchText: String = ""
+    
     private let items: [Photo] = Array(1...100).map { i in
         Photo(id: i, title: "item \(i)", url: URL(string: "https://images.pexels.com/photos/3573351/pexels-photo-3573351.png?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=200&w=280")!)
     }
@@ -38,6 +40,12 @@ struct ImageSearchView: View {
                     }
                 }
             }
+            .searchable(
+                text: $searchText,
+                placement: .navigationBarDrawer(displayMode: .always),
+                prompt: "Search by keyword"
+            )
+            .padding()
         }
     }
 }
