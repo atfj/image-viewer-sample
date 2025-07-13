0f2a5dd723d0b39a0c4302f8b3c6685fcbbd9ab2
atfj <a9fujita@gmail.com>
2025-07-13 15:06:01 +0900

    Add grid iew

--- ImageViewerSample/ImageSearchView.swift
+++ ImageViewerSample/ImageSearchView.swift
@@ -7,15 +7,38 @@
 
 import SwiftUI
 
+struct Photo: Identifiable {
+    let id: Int
+    let title: String
+    let url: URL
+}
+
 struct ImageSearchView: View {
+    private let items: [Photo] = Array(1...100).map { i in
+        Photo(id: i, title: "item \(i)", url: URL(filePath: "")!)
+    }
+    
+    private let columns = Array(
+        repeating: GridItem(.flexible(), spacing: 16),
+        count: 3
+    )
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(items) { item in
+                        VStack {
+                            Rectangle()
+                                .fill(Color.gray)
+                                .frame(width: 100, height: 100)
+                            Text(item.title)
+                                .frame(width: 100)
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
