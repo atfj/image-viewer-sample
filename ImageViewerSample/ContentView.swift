a177ba30b60cc62582986da97cacf55bfcc550c9
atfj <a9fujita@gmail.com>
2025-07-13 11:27:45 +0900

    Initial Commit

---
+++ ImageViewerSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ImageViewerSample
+//
+//  Created by Atsuhiro Fujita on 7/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ImageViewerSample/ImageViewerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewerSampleApp.swift
+//  ImageViewerSample
+//
+//  Created by Atsuhiro Fujita on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
