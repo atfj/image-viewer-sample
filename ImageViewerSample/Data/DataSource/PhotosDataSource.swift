17f1bb7aff81fc163aeec4a8c998a63e375ea8da
atfj <a9fujita@gmail.com>
2025-07-14 00:41:23 +0900

    Implement search endpoint

---
+++ ImageViewerSample/Data/DataSource/PhotosDataSource.swift
@@ -0,0 +1,23 @@
+//
+//  PhotosDataSource.swift
+//  ImageViewerSample
+//
+//  Created by Atsuhiro Fujita on 7/13/25.
+//
+
+protocol PhotosDataSourceProtocol {
+    func search(query: String, page: Int?, perPage: Int?) async throws -> PexelsSearchResponse
+}
+
+class PhotosDataSource: PhotosDataSourceProtocol {
+    let apiClient: ApiClientProtocol
+    
+    init(_ apiClient: ApiClientProtocol = ApiClient()) {
+        self.apiClient = apiClient
+    }
+    
+    func search(query: String, page: Int? = nil, perPage: Int? = nil) async throws -> PexelsSearchResponse {
+        let request = PixelsSearchRequest(query: query, perPage: perPage, page: page)
+        return try await apiClient.request(of: request)
+    }
+}
